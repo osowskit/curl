9bd29c9d832f9b42c3e49be8d223b466ddc6d96d
Thomas Osowski <osowskit@github.com>
2018-09-20 12:35:20 -0700

    Move args to shell

--- curl_entrypoint.sh
+++ curl_entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-echo "$@ $SLACK_WEBHOOK_URL"
-
-curl "$@ $SLACK_WEBHOOK_URL"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintroduce myself"}' "$SLACK_WEBHOOK_URL"
